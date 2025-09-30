8fc80517544b06a4151e5ad66f9c8be8c962483f
Christopher Byrd <chrabyrd@gmail.com>
2025-09-30 12:18:41 -0700

    nit

--- arches_search/migrations/0003_add_advanced_search_facets.py
+++ arches_search/migrations/0003_add_advanced_search_facets.py
@@ -221,12 +221,7 @@ class Migration(migrations.Migration):
             fields=[
                 (
                     "id",
-                    models.BigAutoField(
-                        auto_created=True,
-                        primary_key=True,
-                        serialize=False,
-                        verbose_name="ID",
-                    ),
+                    models.AutoField(primary_key=True, serialize=False),
                 ),
                 (
                     "controlled_list",
