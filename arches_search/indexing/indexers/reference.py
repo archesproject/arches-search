11e90611c1ac526d78ac3b82fdb1e8a0b486fc0a
njkim <nkim@fargeo.com>
2025-10-07 15:09:28 -0700

    Add uuid search to reference, #12

--- arches_search/indexing/indexers/reference.py
+++ arches_search/indexing/indexers/reference.py
@@ -1,7 +1,7 @@
 from django.contrib.postgres.search import SearchVector
 from arches.app.datatypes.datatypes import DataTypeFactory, BaseDataType
 from arches.app.models.models import Language
-from arches_search.models.models import TermSearch
+from arches_search.models.models import TermSearch, UUIDSearch
 
 from arches_search.indexing.base import BaseIndexing
 
@@ -20,7 +20,7 @@ class ReferenceIndexing(BaseIndexing):
     def index(self, tile, node):
         nodeid = str(node.nodeid)
         self._set_languages()
-        document = {"strings": []}
+        document = {"strings": [], "references": []}
         self.datatype.append_to_document(document, tile.data[nodeid], node, tile)
         for string in document["strings"]:
             string_search = TermSearch.objects.create(
@@ -40,3 +40,14 @@ class ReferenceIndexing(BaseIndexing):
             TermSearch.objects.filter(pk=string_search.pk).update(
                 search_vector=string_search.search_vector
             )
+
+        for reference in document["references"]:
+            uuid_search = UUIDSearch.objects.create(
+                node_alias=node.alias,
+                tileid=tile.tileid,
+                resourceinstanceid=tile.resourceinstance_id,
+                datatype=self.datatype.datatype_name,
+                graph_alias=node.graph.slug,
+                value=reference["id"],
+            )
+            uuid_search.save()
