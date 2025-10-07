5763d41eb695dac306a215ec4917bd4e6eb10378
njkim <nkim@fargeo.com>
2025-10-07 15:55:39 -0700

    Add indexer to resource_instance (& list), #12

---
+++ arches_search/indexing/indexers/resource_instance_list.py
@@ -0,0 +1,9 @@
+from arches.app.datatypes.datatypes import DataTypeFactory, BaseDataType
+
+from arches_search.indexing.indexers.resource_instance import ResourceInstanceIndexing
+
+
+class ResourceInstanceListIndexing(ResourceInstanceIndexing):
+    def __init__(self):
+        super().__init__()
+        self.datatype: BaseDataType = DataTypeFactory().get_instance("resource-instance-list")
