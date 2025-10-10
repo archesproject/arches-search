d6a3309258589e4b9ec3cbb8df10a243f445bd40
Christopher Byrd <chrabyrd@gmail.com>
2025-10-10 15:59:23 -0500

    nit

--- arches_search/views/api/advance_search_facet.py
+++ arches_search/views/api/advance_search_facet.py
@@ -7,12 +7,6 @@ from arches_search.models.models import AdvancedSearchFacet
 
 
 class DatatypeFacetsAPI(APIBase):
-    """
-    GET /api/advanced-search/datatypes/<datatype>/facets
-
-    Returns a list of facets for the given datatype, ordered by sortorder (then id).
-    """
-
     def get(self, request, datatype):
         facets = AdvancedSearchFacet.objects.filter(datatype=datatype).order_by(
             "sortorder"
@@ -21,34 +15,8 @@ class DatatypeFacetsAPI(APIBase):
 
 
 class AllDatatypeFacetsAPI(APIBase):
-    """
-    GET /api/advanced-search/facets
-
-    Returns all facets grouped by datatype. Each group's items are ordered by
-    sortorder (then id). The response shape is:
-
-    {
-        "<datatype>": [
-            {
-                "id": ...,
-                "arity": ...,
-                "label": ...,
-                "operator": ...,
-                "param_formats": [...],
-                "sortorder": ...,
-                "orm_template": "...",
-                "is_orm_template_negated": false,
-                "sql_template": "..."
-            },
-            ...
-        ],
-        ...
-    }
-    """
-
     def get(self, request):
-        rows = AdvancedSearchFacet.objects.select_related("datatype").order_by(
+        facets = AdvancedSearchFacet.objects.select_related("datatype").order_by(
             "datatype_id", "sortorder"
         )
-
-        return JSONResponse(rows)
+        return JSONResponse(list(facets))
