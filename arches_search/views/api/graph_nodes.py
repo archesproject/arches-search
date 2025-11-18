06120fc7e4d355206d4693a77f594cb00b0cde37
Christopher Byrd <chrabyrd@gmail.com>
2025-11-18 10:36:34 -0800

    nit

--- arches_search/views/api/graph_nodes.py
+++ arches_search/views/api/graph_nodes.py
@@ -6,7 +6,5 @@ from arches.app.models import models as arches_models
 
 class GraphNodesAPI(APIBase):
     def get(self, request, graph_id):
-
         nodes = arches_models.Node.objects.filter(graph_id=graph_id).order_by("name")
-
         return JSONResponse(list(nodes))
