ec4a73f5beeb72741de3787a4c4f79f816cf4fe4
Aaron Gundel <124614+aarongundel@users.noreply.github.com>
2026-04-09 21:26:56 -0600

    Adds node filter config

---
+++ arches_search/migrations/0018_node_filter_config.py
@@ -0,0 +1,43 @@
+import django.db.models.deletion
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ("models", "0001_initial"),
+        ("arches_search", "0017_advancedsearchfacet_filter_field"),
+    ]
+
+    operations = [
+        migrations.CreateModel(
+            name="NodeFilterConfig",
+            fields=[
+                ("id", models.AutoField(primary_key=True, serialize=False)),
+                ("config", models.JSONField(default=dict)),
+                (
+                    "slug",
+                    models.TextField(default="filtering"),
+                ),
+                (
+                    "graph",
+                    models.ForeignKey(
+                        on_delete=django.db.models.deletion.CASCADE,
+                        related_name="node_filter_configs",
+                        to="models.graphmodel",
+                    ),
+                ),
+            ],
+            options={
+                "db_table": "arches_search_node_filter_config",
+                "managed": True,
+            },
+        ),
+        migrations.AddConstraint(
+            model_name="nodefilterconfig",
+            constraint=models.UniqueConstraint(
+                fields=["graph", "slug"],
+                name="unique_node_filter_config_slug_per_graph",
+            ),
+        ),
+    ]
