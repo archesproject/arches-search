88fc14b6a131e77b1589862dc4d19f3811bf23e9
Christopher Byrd <chrabyrd@gmail.com>
2026-07-24 14:48:24 -0700

    updates term search interaction

--- arches_search/views/api/simple_search.py
+++ arches_search/views/api/simple_search.py
@@ -6,8 +6,8 @@ from arches.app.views.api import APIBase
 
 from arches_search.utils.search_aggregation import build_aggregations
 from arches_search.utils.search_queryset import (
+    SimpleSearchQuerysetBuilder,
     build_resource_type_counts,
-    build_search_queryset,
 )
 from arches_search.utils.search_sort import SortResolver
 
@@ -15,14 +15,23 @@ from arches_search.utils.search_sort import SortResolver
 class SimpleSearchAPI(APIBase):
     def post(self, request):
         body = JSONDeserializer().deserialize(request.body)
-        results_queryset = build_search_queryset(body)
+        querysets = SimpleSearchQuerysetBuilder(body)
 
-        results_queryset = SortResolver(body.get("sort")).apply(results_queryset)
+        results_queryset = SortResolver(body.get("sort")).apply(
+            querysets.scoped_queryset
+        )
+
+        resource_type_counts, all_resource_count = build_resource_type_counts(
+            body.get("terms"), querysets.type_agnostic_queryset
+        )
 
         page_number = body.get("page", 1)
         page_size = body.get("page_size", 20)
 
         paginator = Paginator(results_queryset, page_size)
+        if not body.get("graphId"):
+            paginator.count = all_resource_count
+
         results_page = paginator.page(page_number)
 
         raw_aggregations = body.get("aggregations")
@@ -30,8 +39,6 @@ class SimpleSearchAPI(APIBase):
         aggregations = {}
         if raw_aggregations:
             aggregations = build_aggregations(results_queryset, raw_aggregations)
-
-        resource_type_counts, all_resource_count = build_resource_type_counts(body)
 
         return JSONResponse(
             {
